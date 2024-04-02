ee35b8467adc476552e960acfc289035e7d6933b
jcari-dev <582119jc@gmail.com>
2024-04-02 13:09:33 +0000

    pushing?

--- chess-django/api/views.py
+++ chess-django/api/views.py
@@ -3,8 +3,9 @@ from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.middleware.csrf import get_token
 from django.shortcuts import render
+from django_ratelimit.decorators import ratelimit
 
-
+@ratelimit(key="ip", rate="3/s", block=True)
 @require_http_methods(["POST", "GET"])
 def vitals(request):
     if request.method == "GET":
@@ -13,11 +14,13 @@ def vitals(request):
         return JsonResponse({"message": "Kicking!"})
 
 
+@ratelimit(key="ip", rate="3/s", block=True)
 @ensure_csrf_cookie
 def get_csrf(request):
     return JsonResponse({"csrfToken": get_token(request)})
 
 
+@ratelimit(key="ip", rate="3/s", block=True)
 @ensure_csrf_cookie
 def set_csrf_token(request):
     return render(request, "api/set_csrf_token.html")
